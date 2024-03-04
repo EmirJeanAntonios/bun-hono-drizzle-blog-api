02bbf0531d360572ace28d3f58039ebb226cef9e
Emir Jean Antonios <emir.antonios@gmail.com>
2024-03-04 17:04:42 +0300

    fix: insert fixed

--- src/routes/blogs.ts
+++ src/routes/blogs.ts
@@ -13,6 +13,15 @@ blogRoute.get("/", async (c) => {
   return c.json(blogs);
 });
 
+blogRoute.get("/:id", async (c) => {
+  const id = c.req.param("id") as string;
+  const blog = await db
+    .select()
+    .from(blogSchema.blogs)
+    .where(eq(blogSchema.blogs.id, parseInt(id)));
+  return c.json(blog);
+});
+
 blogRoute.post(
   "/save",
   validator("form", (value, c) => {
@@ -36,11 +45,17 @@ blogRoute.post(
         .where(eq(blogSchema.blogs.id, value?.id));
 
       if (blog.length == 0) {
-        await db.insert(blogSchema.blogs).values(value);
+        await db.insert(blogSchema.blogs).values({
+          ...value,
+          id: undefined,
+        });
         return c.json({ message: "Successfuly created!" });
       }
 
-      await db.update(blogSchema.blogs).set(value).where(eq(blogSchema.blogs.id, value?.id));
+      await db
+        .update(blogSchema.blogs)
+        .set(value)
+        .where(eq(blogSchema.blogs.id, value?.id));
       return c.json({ message: "Successfuly updated!" });
     }
 
